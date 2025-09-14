c25e3d360e01c29c911c2ff317282e45f573b171
tor <torverner@proton.me>
2025-09-14 13:14:48 +0200

    added some testcases

--- pkg/ez/addon_types.go
+++ pkg/ez/addon_types.go
@@ -1,6 +1,8 @@
 package ez
 
 import (
+	"encoding/json"
+
 	"github.com/spf13/afero"
 )
 
@@ -8,6 +10,33 @@ type PortConfig struct {
 	NodePort int    `json:"nodePort"`
 	HostPort int    `json:"hostPort"`
 	Protocol string `json:"protocol"`
+}
+
+func (pc *PortConfig) UnmarshalJSON(data []byte) error {
+	// Using a helper struct to avoid recursion during unmarshaling
+	type Alias PortConfig
+	aux := &struct {
+		*Alias
+	}{
+		Alias: (*Alias)(pc),
+	}
+
+	// Set default values
+	aux.NodePort = 0     // Assuming 0 is acceptable as a default
+	aux.HostPort = 0     // Assuming 0 is acceptable as a default
+	aux.Protocol = "TCP" // Default protocol set to "TCP"
+
+	// Unmarshal JSON data into the aux struct
+	if err := json.Unmarshal(data, aux); err != nil {
+		return err
+	}
+
+	// Ensure Protocol is set if not present in JSON
+	if aux.Protocol == "" {
+		aux.Protocol = "TCP"
+	}
+
+	return nil
 }
 
 type MountConfig struct {
---
+++ test_addons/portconfig/missingfield.ek.js
@@ -0,0 +1,7 @@
+// test that validation fails because nodePort is missing
+let configuration = {
+    "extraPorts": [{
+        "containerPort" : "333"
+         }
+    ]
+}
