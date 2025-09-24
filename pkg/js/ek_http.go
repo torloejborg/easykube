14b7cff871c85ff90d4f2a4cec1d0483f6961188
tor <torverner@proton.me>
2025-09-24 22:40:16 +0200

    ek_http follows same pattern as ek_exec

--- pkg/js/ek_http.go
+++ pkg/js/ek_http.go
@@ -3,14 +3,50 @@ package jsutils
 import (
 	"bytes"
 	"fmt"
+	"io"
 	"io/ioutil"
 	"net/http"
 
 	"github.com/dop251/goja"
 )
 
+type HttpResult struct {
+	runtime    *goja.Runtime
+	self       goja.Value
+	success    bool
+	output     string
+	statusCode int
+}
+
+func (er *HttpResult) OnSuccess(call goja.FunctionCall) goja.Value {
+	if er.statusCode >= 200 && er.statusCode < 300 && er.success && len(call.Arguments) == 1 {
+		if fn, ok := goja.AssertFunction(call.Arguments[0]); ok {
+			_, _ = fn(nil, er.runtime.ToValue(er.output))
+		}
+	}
+	return er.self
+}
+
+func (er *HttpResult) OnFail(call goja.FunctionCall) goja.Value {
+	if er.statusCode >= 300 && len(call.Arguments) == 1 {
+		if fn, ok := goja.AssertFunction(call.Arguments[0]); ok {
+			_, _ = fn(nil, er.runtime.ToValue(er.output), er.runtime.ToValue(er.statusCode))
+		}
+	}
+	return er.self
+}
+
 func (ctx *Easykube) Http() func(goja.FunctionCall) goja.Value {
 	return func(call goja.FunctionCall) goja.Value {
+
+		htr := &HttpResult{runtime: ctx.AddonCtx.vm, statusCode: -1}
+		obj := ctx.AddonCtx.NewObject()
+		htr.self = obj
+
+		// bind methods
+		_ = obj.Set("onSuccess", htr.OnSuccess)
+		_ = obj.Set("onFail", htr.OnFail)
+
 		// Extract arguments
 		url := call.Argument(0).String()
 		method := "GET"
@@ -33,28 +69,41 @@ func (ctx *Easykube) Http() func(goja.FunctionCall) goja.Value {
 		// Make the HTTP request
 		req, err := http.NewRequest(method, url, bytes.NewBuffer([]byte(body)))
 		if err != nil {
-			panic(ctx.AddonCtx.vm.NewGoError(err))
-		}
-		for k, v := range headers {
-			req.Header.Set(k, v)
+			htr.output = err.Error()
+			htr.success = false
+			return obj
+
+		} else {
+			for k, v := range headers {
+				req.Header.Set(k, v)
+			}
+
+			client := &http.Client{}
+			resp, err := client.Do(req)
+			if err != nil {
+				htr.output = err.Error()
+				htr.success = false
+				return obj
+			}
+
+			defer func(Body io.ReadCloser) {
+				_ = Body.Close()
+			}(resp.Body)
+
+			respBody, err := ioutil.ReadAll(resp.Body)
+			if err != nil {
+				htr.output = err.Error()
+				htr.success = false
+				htr.statusCode = -1
+				return obj
+			}
+
+			htr.success = true
+			htr.output = string(respBody)
+			htr.statusCode = resp.StatusCode
+
 		}
 
-		client := &http.Client{}
-		resp, err := client.Do(req)
-		if err != nil {
-			panic(ctx.AddonCtx.vm.NewGoError(err))
-		}
-		defer resp.Body.Close()
-
-		respBody, err := ioutil.ReadAll(resp.Body)
-		if err != nil {
-			panic(ctx.AddonCtx.vm.NewGoError(err))
-		}
-
-		// Return the response as a JavaScript object
-		obj := ctx.AddonCtx.vm.NewObject()
-		obj.Set("status", resp.StatusCode)
-		obj.Set("body", string(respBody))
 		return obj
 	}
 }
