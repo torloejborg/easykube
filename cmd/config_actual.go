6cbd2525fd0dfc590b315fb5c5dbdc3b8d362cad
Tor <torverner@proton.me>
2025-11-22 14:22:10 +0100

    enable private registries in configuration

--- cmd/config_actual.go
+++ cmd/config_actual.go
@@ -1,6 +1,9 @@
 package cmd
 
 import (
+	"fmt"
+	"os"
+
 	"github.com/spf13/cobra"
 	"github.com/torloejborg/easykube/pkg/ez"
 )
@@ -15,4 +18,29 @@ func runConfigActual(cmd *cobra.Command, args []string) error {
 	ezk.EditConfig()
 
 	return nil
+}
+
+func patchConfigWithPrivateRegistryTemplate(cfg *ez.EasykubeConfigData) {
+	configStanza := `  
+  # Declare private registries. Whenever easykube pulls an image, and the registry name contains a substring
+  # defined by repositoryMatch, the credentials are resolved by looking up the values in easykube-secrets.
+  #private-registries:
+  #  - repositoryMatch: partial-registry-name.io
+  #    userKey: userCredentialsKey
+  #    passwordKey: userCredentialPasswordKey
+`
+
+	if cfg.PrivateRegistries == nil {
+
+		fmt.Println(cfg.ConfigurationFile)
+
+		// patch config
+		f, err := os.OpenFile(cfg.ConfigurationFile, os.O_APPEND|os.O_WRONLY, os.ModeAppend)
+
+		if err != nil {
+			panic(err)
+		}
+		f.WriteString(configStanza)
+	}
+	fmt.Printf(configStanza)
 }
--- pkg/ez/config_utils.go
+++ pkg/ez/config_utils.go
@@ -7,11 +7,13 @@ import (
 	"os"
 	"os/exec"
 	"path/filepath"
+	"strings"
 	"text/template"
 
 	"github.com/gookit/config/v2"
 	"github.com/gookit/config/v2/yaml"
 	"github.com/torloejborg/easykube/pkg/resources"
+	"github.com/torloejborg/easykube/pkg/textutils"
 )
 
 type PrivateRegistry struct {
@@ -34,6 +36,8 @@ type IEasykubeConfig interface {
 	MakeConfig() error
 	EditConfig()
 	LaunchEditor(config, editor string)
+	PatchConfig() error
+	PathToConfigFile() string
 }
 
 type EasykubeConfig struct {
@@ -66,9 +70,16 @@ func (ec *EasykubeConfig) LaunchEditor(config, editor string) {
 	}
 }
 
+func (ec *EasykubeConfig) PathToConfigFile() string {
+	configDir, _ := Kube.GetUserConfigDir()
+	return filepath.Join(configDir, ec.ConfigDirName, "config.yaml")
+}
+
 func (ec *EasykubeConfig) LoadConfig() (*EasykubeConfigData, error) {
 	// if no config file, create a default
-	ec.MakeConfig()
+	if err := ec.MakeConfig(); err != nil {
+		return nil, err
+	}
 
 	config.ClearAll()
 	// config.ParseEnv: will parse env var in string value. eg: shell: ${SHELL}
@@ -76,9 +87,8 @@ func (ec *EasykubeConfig) LoadConfig() (*EasykubeConfigData, error) {
 
 	// add driver for support yaml content
 	config.AddDriver(yaml.Driver)
-	configDir, _ := Kube.GetUserConfigDir()
 
-	data, err := ReadFileToBytes(filepath.Join(configDir, ec.ConfigDirName, "config.yaml"))
+	data, err := ReadFileToBytes(ec.PathToConfigFile())
 	if err != nil {
 		return nil, err
 	}
@@ -104,17 +114,18 @@ func (ec *EasykubeConfig) EditConfig() {
 		fmt.Println("VISUAL environment variable not set")
 		os.Exit(-1)
 	} else {
-		configDir, err := os.UserConfigDir()
-		pathToConfig := filepath.Join(configDir, ec.ConfigDirName, "config.yaml")
+		_, err := os.UserConfigDir()
 
 		if err != nil {
 			fmt.Println("Could not determine $HOMEDIR")
 			os.Exit(-1)
 		}
 
-		ec.MakeConfig()
-		ec.LaunchEditor(pathToConfig, editor)
-
+		if err := ec.MakeConfig(); err == nil {
+			ec.LaunchEditor(ec.PathToConfigFile(), editor)
+		} else {
+			Kube.FmtRed("Failed to launch editor: %s", err.Error())
+		}
 	}
 }
 
@@ -129,7 +140,7 @@ func (ec *EasykubeConfig) MakeConfig() error {
 		return err
 	}
 
-	pathToConfigFile := filepath.Join(userConfigDir, ec.ConfigDirName, "config.yaml")
+	pathToConfigFile := ec.PathToConfigFile()
 	_, err = Kube.Fs.Stat(pathToConfigFile)
 
 	if os.IsNotExist(err) {
@@ -190,4 +201,54 @@ func (ec *EasykubeConfig) MakeConfig() error {
 	}
 
 	return nil
+}
+
+func (ec *EasykubeConfig) PatchConfig() error {
+	cfg, _ := ec.LoadConfig()
+	ec.patchConfigWithPrivateRegistryTemplate(cfg)
+
+	return nil
+}
+
+func (ec *EasykubeConfig) patchConfigWithPrivateRegistryTemplate(cfg *EasykubeConfigData) {
+
+	configStanza := textutils.TrimMargin(`
+	  |  #Declare private registries. Whenever easykube pulls an image, and the registry name contains a substring
+	  |  #defined by repositoryMatch, the credentials are resolved by looking up the values in easykube-secrets.
+	  |  #private-registries:
+	  |   #- repositoryMatch: partial-registry-name.io
+	  |   #  userKey: userCredentialsKey
+	  |   #  passwordKey: userCredentialPasswordKey`, "|")
+
+	cfgFile := ec.PathToConfigFile()
+
+	data, _ := ReadFileToBytes(cfgFile)
+	cfgText := string(data)
+
+	if cfg.PrivateRegistries == nil && !strings.Contains(cfgText, configStanza) {
+
+		// patch config
+		f, err := os.OpenFile(cfgFile, os.O_APPEND|os.O_WRONLY, os.ModeAppend)
+		defer func(f *os.File) {
+			err := f.Close()
+			if err != nil {
+				panic(err)
+			}
+		}(f)
+
+		if err != nil {
+			panic(err)
+		}
+		_, err = f.WriteString(configStanza)
+		if err != nil {
+			return
+		}
+
+		Kube.FmtGreen("Your configuration was patched with the following stanza")
+		fmt.Println()
+		Kube.FmtGreen(configStanza)
+		fmt.Println()
+		Kube.FmtGreen("Edit your configuration to enable private registries")
+		fmt.Println()
+	}
 }
