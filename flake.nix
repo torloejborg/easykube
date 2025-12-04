cd0d78007eca4bdb468b5cfe3f4b29c2ca410296
tor <torverner@proton.me>
2025-12-04 22:34:02 +0100

    nix flake

--- flake.nix
+++ flake.nix
@@ -9,84 +9,95 @@
   outputs = { self, nixpkgs, unstable }:
     let
       system = "x86_64-linux";
+
       pkgs = nixpkgs.legacyPackages.${system};
       pkgsUnstable = unstable.legacyPackages.${system};
-      lib = pkgs.lib;
-      go_compiler_flags = "-tags=remote,containers_remote,exclude_graphdriver_btrfs,exclude_graphdriver_devicemapper,exclude_graphdriver_overlay,exclude_graphdriver_zfs";
 
-        # Common packages used in all shells
-            commonPackages = with pkgs; [
-              (ruby.withPackages (ps: with ps; [ rouge  ]))
-              jq
-              yq
-              gnumake
-              glibcLocales
-              pkg-config
-              stdenv.cc
-              gpgme
-              libgpg-error
-            ] ++ [
-              pkgsUnstable.upx
-              pkgsUnstable.mockgen
-              pkgsUnstable.kubectl
-              pkgsUnstable.kubernetes-helm
-              pkgsUnstable.kustomize
-              pkgsUnstable.go_1_24
-            ] ;
+      go_tags = [
+        "remote"
+        "containers_remote"
+        "exclude_graphdriver_btrfs"
+        "exclude_graphdriver_devicemapper"
+        "exclude_graphdriver_overlay"
+        "exclude_graphdriver_zfs"
+      ];
+
+      go_flags = "-tags=${builtins.concatStringsSep "," go_tags}";
+
+      commonPackages = with pkgs; [
+        (ruby.withPackages (ps: with ps; [ rouge ]))
+        jq
+        yq
+        gnumake
+        glibcLocales
+        pkg-config
+        stdenv.cc
+        gpgme
+        libgpg-error
+        btrfs-progs
+        lvm2
+      ] ++ [
+        pkgsUnstable.upx
+        pkgsUnstable.mockgen
+        pkgsUnstable.kubectl
+        pkgsUnstable.kubernetes-helm
+        pkgsUnstable.kustomize
+        pkgsUnstable.go_1_24
+      ];
     in {
       packages.${system}.default = pkgsUnstable.buildGoModule {
         pname = "easykube";
         version = "latest";
         src = self;
-         nativeBuildInputs = [
-           pkgs.pkg-config
-           pkgs.stdenv.cc
-         ];
 
-         buildInputs = [
-            pkgs.gpgme
-            pkgs.libgpg-error
-         ];
+        # EXPLICITLY disable vendor mode (important for some nixpkgs revisions)
+        vendorHash = null;
 
-          tags = [
-            "remote"
-            "containers_remote"
-            "exclude_graphdriver_btrfs"
-            "exclude_graphdriver_devicemapper"
-            "exclude_graphdriver_overlay"
-            "exclude_graphdriver_zfs"
-          ];
+        # Use module-download mode — set to "" and let nix tell you the correct hash
+        modSha256 = "";
 
-        #vendorHash = "sha256-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=";
-        vendorHash = "sha256-DuRFwAZzu7XXXRFrxFM2t2RH48HKKw/th0n+tVzVGBU=";
+        nativeBuildInputs = [
+          pkgs.pkg-config
+          pkgs.stdenv.cc
+        ];
+
+        buildInputs = [
+          pkgs.gpgme
+          pkgs.libgpg-error
+          pkgs.btrfs-progs
+          pkgs.lvm2
+        ];
+
+        tags = go_tags;
+
+        # ensure go runs in module mode (optional/redundant with vendorHash=null)
+        buildFlags = [ "-mod=mod" ];
       };
 
       devShells.${system} = {
         default = pkgs.mkShell {
-
-          packages = commonPackages ++  [
-            (self.packages.${system}.default)
-          ];
+          packages = commonPackages;
 
           shell = pkgs.zsh;
           impureEnv = true;
+
           shellHook = ''
             export LC_ALL=C.UTF-8
             export LANG=C.UTF-8
             export PS1="[ek-dev]> "
-            source <(easykube completion bash)
-
             echo "Welcome to the easykube dev shell"
             echo
-            easykube
           '';
         };
 
         light = pkgs.mkShell {
-          GOFLAGS = go_compiler_flags;
+          GOFLAGS = go_flags;
+
           packages = commonPackages;
+
           shell = pkgs.zsh;
           impureEnv = true;
+
           shellHook = ''
             export LC_ALL=C.UTF-8
             export LANG=C.UTF-8
