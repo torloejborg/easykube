bbe0b7d7fa85047e128ca8edc3143d8553b9ad84
tor <torverner@proton.me>
2025-12-15 14:17:16 +0100

    update flake

--- flake.nix
+++ flake.nix
@@ -36,29 +36,34 @@
         #vendorHash = "sha256-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=";
         vendorHash = "sha256-vuwzjHu0VaewO7Di70HfcHwAxdTdVq0N0+Vy3ktgX5E=";
 
+          ldflags = [
+            "-s"
+            "-w"
+            "-extldflags=-static"
+          ];
       };
 
       devShells.${system} = {
-        default = pkgs.mkShell {
+       default = pkgs.mkShell {
+         inputsFrom = [
+           self.packages.${system}.default
+         ];
 
-          packages = commonPackages ++  [
-            (self.packages.${system}.default)
-          ];
+         packages = commonPackages;
 
-          shell = pkgs.zsh;
-          impureEnv = true;
-          shellHook = ''
-            export LC_ALL=C.UTF-8
-            export LANG=C.UTF-8
-            export PS1="[ek-dev]> "
-            source <(easykube completion bash)
+         shell = pkgs.zsh;
+         impureEnv = true;
+         shellHook = ''
+           export LC_ALL=C.UTF-8
+           export LANG=C.UTF-8
+           export PS1="[ek-dev]> "
+           source <(easykube completion bash)
 
-            echo "Welcome to the easykube dev shell"
-            echo
-            easykube
-          '';
-        };
-
+           echo "Welcome to the easykube dev shell"
+           echo
+           easykube
+         '';
+       };
         light = pkgs.mkShell {
           packages = commonPackages;
           shell = pkgs.zsh;
