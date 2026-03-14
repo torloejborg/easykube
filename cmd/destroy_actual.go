ee31f9b49676e501b57af70c35f2147b3a416e0e
tor <torverner@proton.me>
2026-03-14 11:36:27 +0100

    checkpoint

--- cmd/destroy_actual.go
+++ cmd/destroy_actual.go
@@ -5,62 +5,51 @@ import (
 	"github.com/torloejborg/easykube/pkg/ez"
 )
 
-/*
-1 .stop control plane
-2. delete registry
-3. delete config
-4. delete persistence
-*/
-
 func destroyActual() error {
-
 	ezk := ez.Kube
-	if search, err := ezk.FindContainer(constants.KIND_CONTAINER); err != nil {
+
+	err := stopAndDeleteContainer(constants.REGISTRY_CONTAINER)
+	if nil != err {
 		return err
-	} else if search.Found {
+	}
 
-		if search.IsRunning {
-
-			if _, err := ezk.FmtSpinner(func() (any, error) {
-				return nil, ezk.StopContainer(search.ContainerID)
-			}, "Stopping %s", constants.KIND_CONTAINER); err != nil {
-				return err
-			}
-		}
-
-		if _, err := ezk.FmtSpinner(func() (any, error) {
-			return nil, ezk.RemoveContainer(search.ContainerID)
-		}, "Removing %s", constants.KIND_CONTAINER); err != nil {
-			return err
-		}
+	err = stopAndDeleteContainer(constants.KIND_CONTAINER)
+	if nil != err {
+		return err
 	}
 
 	if ezk.GetBoolFlag("purge") {
 
-		if search, err := ezk.FindContainer(constants.REGISTRY_CONTAINER); err != nil {
+		cfg, err := ezk.LoadConfig()
+		if err != nil {
 			return err
-		} else if search.Found {
-			if search.IsRunning {
-				if _, err := ezk.FmtSpinner(func() (any, error) {
-					return nil, ezk.StopContainer(search.ContainerID)
-				}, "Stopping %s", constants.REGISTRY_CONTAINER); err != nil {
-					return err
-				}
-			}
+		}
+
+		err = ezk.Fs.RemoveAll(cfg.ConfigurationDir)
+		if err != nil {
+			ezk.FmtYellow("could not remove configuration dir: %s", cfg.ConfigurationDir)
+		}
+	}
+
+	return nil
+}
+
+func stopAndDeleteContainer(name string) error {
+	ezk := ez.Kube
+	if search, err := ezk.FindContainer(name); err != nil {
+		return err
+	} else if search.Found {
+		if search.IsRunning {
 			if _, err := ezk.FmtSpinner(func() (any, error) {
-				return nil, ezk.RemoveContainer(search.ContainerID)
-			}, "Removing %s", constants.KIND_CONTAINER); err != nil {
+				return nil, ezk.StopContainer(search.ContainerID)
+			}, "Stopping %s", name); err != nil {
 				return err
 			}
 		}
-		cfg, e := ezk.LoadConfig()
-		if e != nil {
-			return e
-		}
-
-		e = ezk.Fs.RemoveAll(cfg.ConfigurationDir)
-		if e != nil {
-			ezk.FmtYellow("could not remove configuration dir: %s", cfg.ConfigurationDir)
+		if _, err := ezk.FmtSpinner(func() (any, error) {
+			return nil, ezk.RemoveContainer(search.ContainerID)
+		}, "Removing %s", name); err != nil {
+			return err
 		}
 	}
 
